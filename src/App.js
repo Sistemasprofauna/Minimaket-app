d46fafc8b1ad4a656ea86df7ef078564d46a783f
Sistemasprofauna <104469367+Sistemasprofauna@users.noreply.github.com>
2022-06-16 20:04:07 -0700

    actualizacion 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./componentes/barraNav";
 //import CrudApp from "./componentes/crud-app";
 import CrudApi from "./componentes/CrudApi";
-import "bootstrap/dist/css/bootstrap.min.css"
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
 
 
